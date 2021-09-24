a61a86cebf5bc1b759252a3dab80d037c0aaead1
kenny <p5323652@gmail.com>
2021-09-24 20:41:33 +0800

    practice

---
+++ practice/sum.js
@@ -0,0 +1,15 @@
+console.log("hello world!");
+
+function sum(param) {
+    var sum=0;
+    for (var i = 0; i < param.length; i++) {
+        sum += param[i];
+    };
+    return sum;
+}
+
+var data=[1,2,3,4]
+console.log(SumData(data));
+
+
+//好了以後 add commit push
